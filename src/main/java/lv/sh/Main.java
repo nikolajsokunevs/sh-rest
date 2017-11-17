01edaf258e6452c958ca4fb8c9bd17cb17d31f73
Nikolay Okunev <nikolay.okunev@ctco.lv>
2017-11-17 16:14:59 +0200

    ok: change web server initialization

--- src/main/java/lv/sh/Main.java
+++ src/main/java/lv/sh/Main.java
@@ -20,7 +20,7 @@ public class Main {
         host = Optional.ofNullable(System.getenv("0.0.0.0"));
         port = Optional.ofNullable(System.getenv("PORT"));
         path = "myapp";
-        BASE_URI = protocol + host.orElse("localhost") + ":" + port.orElse("8080") + "/" + path + "/";
+        BASE_URI = protocol + host.orElse("0.0.0.0") + ":" + port.orElse("8080") + "/" + path + "/";
     }
     public static HttpServer startServer() {
         final ResourceConfig rc = new ResourceConfig().packages("lv.sh.resources");
